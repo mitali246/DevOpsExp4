b3a2cb3f90b26f4def4f64518b0187d4ba714dc4
mitali246 <66620428+mitali246@users.noreply.github.com>
2025-08-31 19:17:28 +0530

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -2,10 +2,7 @@ import pytest
 from selenium import webdriver
 
 def test_website_title():
-    driver = webdriver.Chrome() 
-
+    driver = webdriver.Safari()   # switched from Chrome → Safari
     driver.get("https://www.google.com")
-
     assert "Google" in driver.title
-
     driver.quit()
